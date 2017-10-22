f16429569124a6d1ec7dad632a7780420aeb0252
David Kao <david8018d@gmail.com>
2017-10-22 14:00:51 +0800

    first push

---
+++ js/common.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	
+	
+	
+	//checkbox radiobtn
+	$(".ll-radio-custom").labelauty();
+	
+	//counter
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1200
+	});
+	
+	//init wow
+	new WOW().init();
+	
+	
+	//hover function
+	$( "[hov]" ).hover(function(){$(this).stop().fadeTo(100,$(this).attr('hov'));},function(){$(this).stop().fadeTo(100,1)});
+	$( "[voh]" ).each(function(){
+		voh = $(this).attr('voh');
+		$(this).css('opacity',voh);
+		})
+	$( "[voh]" ).hover(function(){$(this).stop().fadeTo(100,1)},function(){$(this).stop().fadeTo(100,$(this).attr('voh'));});
+
+	//menu scrolling
+	$(window).scroll(function() {
+		var windscroll = $(window).scrollTop();
+		if (windscroll >= 180) {
+						
+			$('header .fixarea').addClass('headerfix');
+	
+		} else {
+			$('header .fixarea').removeClass('headerfix');
+		}	
+	
+	}).scroll();
+	
+	//flex
+	$('.flexslider').flexslider({
+		animation: "fade",
+		slideshow:true,
+		slideshowSpeed:4200,
+		animationSpeed:800,
+		controlNav:false,
+		start: function(slider){
+		  $('body').removeClass('loading');
+		}
+	});
+	
+	//visual_center
+	$('.v-centerimg').centerImage();
+	$('.v-centerimg-is').centerImage('inside');
+	
+	$(window).resize(function() {
+		$('.v-centerimg').centerImage();
+		$('.v-centerimg-is').centerImage('inside');
+	});
+	
+	$('select').niceSelect();
+					   
+});
